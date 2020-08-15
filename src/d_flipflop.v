ade464bdf64c496f9964dd58123166e5e1a39eea
bryanwofter <4280556+bryanwofter@users.noreply.github.com>
2020-08-15 09:29:18 -0400

    Added d flipflop

---
+++ src/d_flipflop.v
@@ -0,0 +1,21 @@
+module d_flipflop (
+  input wire d,
+  input wire clk,
+  input wire rst,
+  output wire q,
+  output wire q_not
+);
+  
+  reg r_q;
+  
+  always @(posedge clk, negedge rst)
+    begin
+      if (~rst)
+        r_q <= 1'b0;
+      else
+        r_q <= d;
+    end
+  
+  assign q = r_q;
+  assign q_not = ~r_q;
+endmodule
